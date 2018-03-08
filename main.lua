413314336c2fab540bdbcc5c35b54797c3465f52
Ryoma Scott <36080724+ryoma1@users.noreply.github.com>
2018-03-08 10:31:52 -0500

    i mean

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+-- Title: TouchAndReact
+-- Name: Ryoma Scott
+-- Course: ICS2O/3C
+-- This program displays a button that when I click 
+--on it, turns colour and displays text and another
+--object.
 
--- Your code here
